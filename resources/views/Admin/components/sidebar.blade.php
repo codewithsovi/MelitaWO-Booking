bcbb354637edcd16b9ca6a6ef07caa4704f0aef4
codewithsovi <sovialfina@gmail.com>
2026-01-23 13:47:06 +0700

    footer, navbar dan sidebar

---
+++ resources/views/Admin/components/sidebar.blade.php
@@ -0,0 +1,43 @@
+ <div class="sidebar pe-4 pb-3">
+    <nav class="navbar bg-secondary navbar-dark">
+        <a href="index.html" class="navbar-brand mx-4 mb-3">
+            <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>Melita WO</h3>
+        </a>
+        <div class="d-flex align-items-center ms-4 mb-4">
+            <div class="position-relative">
+                <img class="rounded-circle" src="{{ asset('darkpan-1.0.0') }}/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
+            </div>
+            <div class="ms-3">
+                <h6 class="mb-0">Jhon Doe</h6>
+                <span>Admin</span>
+            </div>
+        </div>
+        <div class="navbar-nav w-100">
+            <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-th me-2"></i>Event</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="button.html" class="dropdown-item"> <i class="fa fa-keyboard me-2"></i> Paket</a>
+                    <a href="typography.html" class="dropdown-item"><i class="fa fa-th me-2"></i> Vendor</a>
+                </div>
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-chart-bar me-2"></i>Klien</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="signin.html" class="dropdown-item"><i class="fa fa-table me-2"></i> Daftar Klien</a>
+                </div>
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Konten</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="signin.html" class="dropdown-item"><i class="fa fa-table me-2"></i>Galerry</a>
+                    <a href="signin.html" class="dropdown-item"><i class="fa fa-table me-2"></i>Manajemen Konten</a>
+                </div>
+            </div>
+                    <!-- <a href="widget.html" class="nav-item nav-link"><i class="fa fa-laptop me-2"></i> Galleries</a>
+                    <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
+                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a> -->
+        </div>
+    </nav>
+</div>
