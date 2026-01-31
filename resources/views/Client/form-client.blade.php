8d6023902809998a406c073cf7855fdf30a9b9b0
codewithsovi <sovialfina@gmail.com>
2026-01-31 10:35:52 +0700

    login nih

---
+++ resources/views/Client/form-client.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>form klien</h2>
+    <form action="">
+        <label for="nama">Nama</label>
+        <input type="text" name="nama">
+        <label for="email">email</label>
+        <input type="email" name="email">
+        <label for="phone">phone</label>
+        <input type="text" name="phone">
+        <label for="alamat">alamat</label>
+        <textarea name="alamat" id="alamat"></textarea>
+        <label for="nama">Nama</label>
+        <input type="text" name="nama">
+    </form>
+</body>
+</html>
