db06672d6fcbb4fdc79666df909480efb916e4b1
codewithsovi <sovialfina@gmail.com>
2026-02-07 20:30:52 +0700

    tampilan edit

--- app/Http/Controllers/Client/ReviewDataController.php
+++ app/Http/Controllers/Client/ReviewDataController.php
@@ -48,20 +48,105 @@ class ReviewDataController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            'client' => 'required|array',
-            'event' => 'required|array',
-            'vendors' => 'required|array',
+            'client' => 'nullable|array',
+            'paket' => 'nullable|array',
+            'event' => 'nullable|array',
+            'groom' => 'nullable|array',
+            'bride' => 'nullable|array',
+            'concept' => 'nullable|array',
+            'vendors' => 'nullable|array',
         ]);
         
         session([
             'booking' => [
                 'client' => $request->client,
+                'paket' => $request->paket,
                 'event' => $request->event,
+                'groom' => $request->groom,
+                'bride' => $request->bride,
+                'concept' => $request->concept,
                 'vendors' => $request->vendors,
             ]
         ]);
+        
+        // dd(session()->all());
 
-        return redirect()->route('booking.review');
+        return redirect()->route('review-data');
     }
+
+    public function final_submit(){
+        $booking = session('booking');
+
+        DB::transaction(function () use ($booking) {
+
+            $client = $this->storeClient($booking);
+            $acara  = $this->storeAcara($booking, $client->id);
+
+            $this->storePengantin($booking, $acara->id);
+            $this->storeKonsep($booking, $acara->id);
+            $this->storeVendors($booking, $acara->id);
+        });
+
+        session()->forget('booking');
+
+        return redirect()->route('booking.success');
+    }
+
+    private function storeClient($booking){
+        return Client::create([
+            'nama' => $booking['client']['nama_client'],
+            'email' => $booking['client']['email'],
+            'no_wa' => $booking['client']['phone'],
+            'alamat' => $booking['client']['alamat'],
+            'paket_id' => $booking['paket']['package_id'],
+            'status' => 'diproses',
+        ]);
+    }
+
+
+    private function storeAcara($booking, $clientId){
+        return Acara::create([
+                'tanggal' => $booking['event']['tanggal_acara'],
+                'lokasi' => $booking['event']['lokasi_acara'],
+                'catatan_tambahan' => $booking['event']['catatan_tambahan'],
+                'klien_id' => $clientId,
+            ]);
+        }
+
+        private function storePengantin($booking, $acaraId)
+        {
+            foreach (['groom', 'bride'] as $role) {
+                Pengantin::create([
+                    'jenis_kelamin' => $role === 'groom' ? 'pria' : 'wanita',
+                    'nama_lengkap' => $booking[$role]['nama_lengkap'],
+                    'nama_panggilan' => $booking[$role]['nama_panggilan'],
+                    'nama_ayah' => $booking[$role]['nama_ayah'],
+                    'nama_ibu' => $booking[$role]['nama_ibu'],
+                    'anak_ke' => $booking[$role]['anak_ke'],
+                    'acara_id' => $acaraId,
+                ]);
+            }
+        }
+
+    private function storeKonsep($booking, $acaraId)
+    {
+        Concept::create([
+            'konsep' => $booking['concept']['nama_konsep'],
+            'link_referensi' => $booking['concept']['link_referensi'],
+            'acara_id' => $acaraId,
+        ]);
+    }
+    private function storeVendors($booking, $acaraId)
+    {
+        foreach ($booking['vendors'] as $vendor) {
+            DetailVendor::create([
+                'vendor_id' => $vendor['vendor_id'],
+                'nama_vendor' => $vendor['nama_vendor'],
+                'sosial_media' => $vendor['kontak'],
+                'acara_id' => $acaraId,
+            ]);
+        }
+    }
+
 
 }
--- resources/views/Client/ReviewData/index.blade.php
+++ resources/views/Client/ReviewData/index.blade.php
@@ -17,20 +17,20 @@
     <p>{{ $booking['client']['alamat'] ?? '-' }}</p>
 
     <h4>Data Event</h4>
-    <p>{{ $booking['event']['tanggal_acara']}}</p>
+    <p>{{ $booking['event']['tanggal_acara'] ?? '-'  }}</p>
     <p>{{ $booking['event']['waktu_acara']}}</p>
     <p>{{ $booking['event']['lokasi_acara']}}</p>
     <p>{{ $booking['event']['catatan_tambahan']}}</p>
 
     <h4>Konsep</h4>
-    <p>{{ $booking['concept']['nama_konsep']}}</p>
-    <p>{{ $booking['concept']['deskripsi']}}</p>
-    <p>{{ $booking['concept']['link_referensi']}}</p>
+    <p>{{ $booking['concept']['nama_konsep'] ?? '-' }}</p>
+    <p>{{ $booking['concept']['deskripsi'] ?? '-' }}</p>
+    <p>{{ $booking['concept']['link_referensi'] ?? '-' }}</p>
 
     <h4>Data Pengantin Pria</h4>
     <p>{{ $booking['groom']['nama_lengkap']}}</p>
     <p>{{ $booking['groom']['nama_panggilan']}}</p>
-    <p>{{ $booking['groom']['alamat']}}</p>
+    <p>{{ $booking['groom']['alamat'] ?? '-' }}</p>
     <p>{{ $booking['groom']['nama_ayah']}}</p>
     <p>{{ $booking['groom']['nama_ibu']}}</p>
     <p>{{ $booking['groom']['anak_ke']}}</p>
@@ -53,6 +53,7 @@
     @endforeach
 
    <a href="{{route('edit-data')}}">Edit</a>
+   <a href=""></a>
 </body>
 
 </html>
